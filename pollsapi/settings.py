cdb9cb26d09b70e2bc12adb3d15f4d8664f8fc15
Rafael <rafaelarturo16@gmail.com>
2019-02-26 11:18:30 -0400

    nuevo

--- pollsapi/settings.py
+++ pollsapi/settings.py
@@ -63,7 +63,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'pollsapi.urls'
 
